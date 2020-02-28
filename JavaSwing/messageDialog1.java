f932f7f3ba201543c70c94df00de5aa34491780b
tariful-fahim <61591757+tariful-fahim@users.noreply.github.com>
2020-02-28 21:04:13 +0600

    Add files via upload
    Show Message Dialog window

--- JavaSwing/messageDialog1.java
+++ JavaSwing/messageDialog1.java
@@ -1,1 +1,19 @@
+/*
+	JOptionPane class and showMessageDialog method
 
+   1st parameter -> positioning my Dialog box
+   2nd parameter -> my message
+   3rd parameter -> Title of the dialog box
+   4th parameter -> icon of the dialog box [-1,3]
+
+*/
+
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
+class messagedialog1{
+	public static void main(String[] args){
+		ImageIcon img = new ImageIcon("monster.jpeg");
+		JOptionPane.showMessageDialog(null, "you win!", "Escape game", -1, img);
+	}
+}
